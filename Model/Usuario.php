f79154a00b42aa6a84ab946013e2d9a5a8b5c960
Edberto Leonardo <edbleonardo@gmail.com>
2020-05-07 16:51:52 -0300

    Mandando projeto pro Git

---
+++ Model/Usuario.php
@@ -0,0 +1,42 @@
+<?php
+
+class Usuario {
+
+    private $nome;
+    private $email;
+    private $senha;
+    private $data;
+
+
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    function getEmail() {
+        return $this->email;
+    }
+
+    function getSenha() {
+        return $this->senha;
+    }
+
+    function getData() {
+        return $this->data;
+    }
+
+    function setEmail($email) {
+        $this->email = $email;
+    }
+
+    function setSenha($senha) {
+        $this->senha = $senha;
+    }
+
+    function setData($data) {
+        $this->data = $data;
+    }
+}
